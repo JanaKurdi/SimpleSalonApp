78e9e422954c6b1167e05d32acfb466d9feac1f2
renad ghaleb saad <rsaad0017@stu.kau.edu.sa>
2022-12-25 21:32:12 +0300

    renad ,this is my first commit

---
+++ src/Client/Customer.java
@@ -0,0 +1,32 @@
+package Client;
+
+public class Customer {
+
+    String username;
+    String email;
+
+
+    public Customer(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public static Customer createAccount(String username, String pass, String repass, String email) throws ExsistedAccountException {
+        DB db = new DB();
+        FileDB fileDB = new FileDB();
+        if (db.checkifExist(username) || fileDB.checkifExist(username)) {
+            throw new ExsistedAccountException();
+        }
+
+        db.createAccount(username, pass, email);
+        fileDB.createAccount(username, pass, email);
+
+        return getCustomer(username, pass);
+    }
+
+    public static Customer getCustomer(String username, String pass) {
+        DB db = new DB();
+        return db.getStudent(username, pass);
+    }
+
+}
